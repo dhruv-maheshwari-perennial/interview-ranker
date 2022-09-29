96eff9b0649d3478a30fa99ce003c164d42397d6
Dhruv Maheshwari <dhruv.maheshwari@perennialsys.com>
2022-09-29 09:24:38 +0530

    deleted feedback files

--- interview-ranker/src/main/java/com/ranker/interviewranker/service/FeedbackService.java
+++
@@ -1,7 +0,0 @@
-package com.ranker.interviewranker.service;
-
-import org.springframework.http.ResponseEntity;
-
-public interface FeedbackService {
-    ResponseEntity<Object> addFeedback(String candidateName, Object feedback);
-}
