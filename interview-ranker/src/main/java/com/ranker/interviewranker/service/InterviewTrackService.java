ee767850a70b2bf3363a11f9ec95402095e02a83
Dhruv Maheshwari <dhruv.maheshwari@perennialsys.com>
2022-10-10 14:32:05 +0530

    added method in interface

--- interview-ranker/src/main/java/com/ranker/interviewranker/service/InterviewTrackService.java
+++ interview-ranker/src/main/java/com/ranker/interviewranker/service/InterviewTrackService.java
@@ -1,8 +1,19 @@
 package com.ranker.interviewranker.service;
 
 import com.ranker.interviewranker.domain.dto.InterviewTrackDTO;
+import com.ranker.interviewranker.domain.model.track.InterviewTrackFields;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface InterviewTrackService {
     ResponseEntity<Object> saveInterviewTrackField(InterviewTrackDTO interviewTrackDTO);
+
+    ResponseEntity<Object> getFieldsTrack();
+
+    ResponseEntity<Object> getFieldsByTrack(String interviewTrack);
+
+    ResponseEntity<Object> addFieldsInTracks(String interviewTrack, List<InterviewTrackFields> interviewTrackFields);
+
+    ResponseEntity<Object> deleteFieldsInTracks(String interviewTrack, List<String> labelName);
 }
