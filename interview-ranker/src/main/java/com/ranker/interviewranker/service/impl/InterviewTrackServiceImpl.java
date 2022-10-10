6d8e0707d45b0019049fab7ba51618767b13894e
Dhruv Maheshwari <dhruv.maheshwari@perennialsys.com>
2022-10-10 14:32:33 +0530

    created more functions

--- interview-ranker/src/main/java/com/ranker/interviewranker/service/impl/InterviewTrackServiceImpl.java
+++ interview-ranker/src/main/java/com/ranker/interviewranker/service/impl/InterviewTrackServiceImpl.java
@@ -2,8 +2,10 @@ package com.ranker.interviewranker.service.impl;
 
 import com.ranker.interviewranker.domain.dto.InterviewTrackDTO;
 import com.ranker.interviewranker.domain.error.ErrorEntity;
+import com.ranker.interviewranker.domain.model.track.InterviewTrackFields;
 import com.ranker.interviewranker.domain.response.ErrorResponseHandler;
 import com.ranker.interviewranker.domain.response.ResponseHandler;
+import com.ranker.interviewranker.enums.InterviewTrackEnum;
 import com.ranker.interviewranker.repository.InterviewTrackRepository;
 import com.ranker.interviewranker.service.InterviewTrackService;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -36,12 +40,68 @@ public class InterviewTrackServiceImpl implements InterviewTrackService {
         return ResponseHandler.resHandler("Successfully saved in db", HttpStatus.OK.value(), result, (long) interviewTrackDTO.getInterviewTrackFieldsList().size(), LocalDateTime.now());
     }
 
-    // 1. Add New Field
-    // 2. Bulk Add New Fields
-    // 3. Delete Field
-    // 4. Bulk delete Fields
-    // 5. Update Field
-    // 6. Bulk Update Fields
-    // 7. Get Fields by Track
-    // 8. Get Fields Track
+    @Override
+    public ResponseEntity<Object> getFieldsTrack() {
+        log.debug("Inside Service: getFieldsTrack()");
+        List<InterviewTrackEnum> interviewTrackEnums = new ArrayList<>();
+        interviewTrackRepository.findAll().forEach(interviewTrackDTO -> interviewTrackEnums.add(interviewTrackDTO.getInterviewTrack()));
+
+        if (interviewTrackEnums.isEmpty()) {
+            return ErrorResponseHandler.errorResponse(new ErrorEntity(LocalDateTime.now(), HttpStatus.NO_CONTENT, HttpStatus.NO_CONTENT.toString(), "Interview track cannot be null or empty"));
+        }
+
+        log.info("Interview Track List: " + interviewTrackEnums);
+        return ResponseHandler.resHandler("Successfully fetched from db", HttpStatus.OK.value(), interviewTrackEnums, (long) interviewTrackEnums.size(), LocalDateTime.now());
+    }
+
+    @Override
+    public ResponseEntity<Object> getFieldsByTrack(String interviewTrack) {
+        log.debug("Inside Service: getFieldsByTrack()");
+        InterviewTrackDTO interviewTrackDTOByInterviewTrack = interviewTrackRepository.getInterviewTrackDTOByInterviewTrack(InterviewTrackEnum.valueOf(interviewTrack)).get(0);
+
+        if (interviewTrackDTOByInterviewTrack == null) {
+            return ErrorResponseHandler.errorResponse(new ErrorEntity(LocalDateTime.now(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.toString(), "No interview track fields found for this track: " + InterviewTrackEnum.valueOf(interviewTrack)));
+        }
+
+        log.info("Interview Track Fields List: " + interviewTrackDTOByInterviewTrack);
+        return ResponseHandler.resHandler("Successfully fetched from db", HttpStatus.OK.value(), interviewTrackDTOByInterviewTrack, LocalDateTime.now());
+    }
+
+    @Override
+    public ResponseEntity<Object> addFieldsInTracks(String interviewTrack, List<InterviewTrackFields> interviewTrackFields) {
+        log.debug("Inside Service: addFieldsInTracks()");
+
+        InterviewTrackDTO interviewTrackDTO = interviewTrackRepository
+                .getInterviewTrackDTOByInterviewTrack(InterviewTrackEnum.valueOf(interviewTrack))
+                .get(0);
+
+        if (interviewTrackDTO == null) {
+            return ErrorResponseHandler.errorResponse(new ErrorEntity(LocalDateTime.now(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.toString(), "No interview track fields found for this track: " + InterviewTrackEnum.valueOf(interviewTrack)));
+        }
+
+        interviewTrackDTO.getInterviewTrackFieldsList().addAll(interviewTrackFields);
+        interviewTrackDTO.setUpdatedAt(LocalDateTime.now());
+
+        log.info("Interview Track Fields List: " + interviewTrackDTO);
+        return ResponseHandler.resHandler("Successfully added new interview fields", HttpStatus.OK.value(), interviewTrackDTO, LocalDateTime.now());
+    }
+
+    @Override
+    public ResponseEntity<Object> deleteFieldsInTracks(String interviewTrack, List<String> labelName) {
+        log.debug("Inside Service: deleteFieldsInTracks()");
+
+        InterviewTrackDTO interviewTrackDTO = interviewTrackRepository
+                .getInterviewTrackDTOByInterviewTrack(InterviewTrackEnum.valueOf(interviewTrack))
+                .get(0);
+
+        if (interviewTrackDTO == null) {
+            return ErrorResponseHandler.errorResponse(new ErrorEntity(LocalDateTime.now(), HttpStatus.NOT_FOUND, HttpStatus.NOT_FOUND.toString(), "No interview track fields found for this track: " + InterviewTrackEnum.valueOf(interviewTrack)));
+        }
+
+        interviewTrackDTO.getInterviewTrackFieldsList().removeAll(labelName);
+        interviewTrackDTO.setUpdatedAt(LocalDateTime.now());
+
+        log.info("Interview Track Fields List: " + interviewTrackDTO);
+        return ResponseHandler.resHandler("Successfully deleted interview fields", HttpStatus.OK.value(), interviewTrackDTO, LocalDateTime.now());
+    }
 }
