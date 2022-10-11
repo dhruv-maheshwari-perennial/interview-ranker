706d386516e4f459251051fdc46ec293f22c5a20
Kaustubh Dongare <kaustubh.dongare@perennialsys.com>
2022-10-11 11:43:53 +0530

    added document annotation for mongodb collection

--- interview-ranker/src/main/java/com/ranker/interviewranker/domain/model/InterviewDetails.java
+++ interview-ranker/src/main/java/com/ranker/interviewranker/domain/model/InterviewDetails.java
@@ -3,6 +3,7 @@ package com.ranker.interviewranker.domain.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @ToString
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Document(collection = "interviews")
 public class InterviewDetails {
    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
    private LocalDateTime dateAndTime;
