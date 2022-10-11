0c65351e7a1a965ea2ed31c1d5cc5b812a273c15
Kaustubh Dongare <kaustubh.dongare@perennialsys.com>
2022-10-11 11:43:23 +0530

    updated function

--- interview-ranker/src/main/java/com/ranker/interviewranker/service/impl/ConsumerServiceImpl.java
+++ interview-ranker/src/main/java/com/ranker/interviewranker/service/impl/ConsumerServiceImpl.java
@@ -2,9 +2,9 @@ package com.ranker.interviewranker.service.impl;
 
 import com.ranker.interviewranker.domain.error.ErrorEntity;
 import com.ranker.interviewranker.domain.model.InterviewDetails;
-import com.ranker.interviewranker.domain.model.Interviews;
 import com.ranker.interviewranker.domain.response.ErrorResponseHandler;
 import com.ranker.interviewranker.domain.response.ResponseHandler;
+import com.ranker.interviewranker.repository.InterviewsRepository;
 import com.ranker.interviewranker.service.ConsumerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,22 +20,24 @@ import java.util.List;
 @Slf4j
 public class ConsumerServiceImpl implements ConsumerService {
     // TODO: Move this to application properties
-    private static final String BASE_URL = "http://192.168.2.17:3000/interviews";
+    private static final String BASE_URL = "http://192.168.2.11:3000/interviews";
 
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+    @Autowired
+    private InterviewsRepository interviewsRepository;
+
     @Override
-    public ResponseEntity<Object> getAllInterviews() {
-        Interviews interviews = new Interviews();
+    public ResponseEntity<Object> saveAllFetchInterviews() {
         List<InterviewDetails> allInterviews = List.of(getInterviews());
 
         if (allInterviews.isEmpty()) {
             return ErrorResponseHandler.errorResponse(new ErrorEntity(LocalDateTime.now(), HttpStatus.NO_CONTENT, HttpStatus.NO_CONTENT.toString(), "No interview details fetched"));
         }
 
-        interviews.setInterviews(allInterviews);
-        return ResponseHandler.resHandler("Successfully fetched", HttpStatus.OK.value(), interviews.getInterviews(), (long) allInterviews.size(), LocalDateTime.now());
+        interviewsRepository.saveAll(allInterviews);
+        return ResponseHandler.resHandler("Successfully fetched", HttpStatus.OK.value(), allInterviews, (long) allInterviews.size(), LocalDateTime.now(), 0L, 0L);
     }
 
     private InterviewDetails[] getInterviews() {
