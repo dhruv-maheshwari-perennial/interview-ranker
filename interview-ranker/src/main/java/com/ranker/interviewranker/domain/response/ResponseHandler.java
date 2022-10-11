bc1aaa52a3f7b7a91183a12cb472c29ee99b10e4
Kaustubh Dongare <kaustubh.dongare@perennialsys.com>
2022-10-11 11:45:25 +0530

    updated insertion order and added total number of pages and current page

--- interview-ranker/src/main/java/com/ranker/interviewranker/domain/response/ResponseHandler.java
+++ interview-ranker/src/main/java/com/ranker/interviewranker/domain/response/ResponseHandler.java
@@ -1,33 +1,41 @@
 package com.ranker.interviewranker.domain.response;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ResponseHandler {
-    static final int VERSION = 1;
-    public static ResponseEntity<Object> resHandler(String message, int status, Object responseObj, Long totalCount, LocalDateTime dateTime) {
-        Map<String, Object> res = new HashMap<>();
+    //TODO: MOVE THIS APPLICATION PROPERTIES
+    private static final int VERSION = 1;
+
+    public static ResponseEntity<Object> resHandler(String message, int status, Object responseObj, Long totalCount, @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS") LocalDateTime dateTime, Long numberOfItems, Long numberOfPage) {
+        Map<String, Object> res = new LinkedHashMap<>();
         res.put("status", status);
+        res.put("totalCount", totalCount);
+        res.put("body", responseObj);
         res.put("message", message);
-        res.put("data", responseObj);
+        res.put("date", dateTime);
         res.put("version", VERSION);
-        res.put("total count", totalCount);
-        res.put("responseDate", dateTime);
+        res.put("currentPage", numberOfPage);
+        res.put("totalPages", numberOfItems);
 
         return new ResponseEntity<>(res, HttpStatus.valueOf(status));
     }
 
-    public static ResponseEntity<Object> resHandler(String message, int status, Object responseObj, LocalDateTime dateTime) {
-        Map<String, Object> res = new HashMap<>();
+    public static ResponseEntity<Object> resHandler(String message, int status, Object responseObj, @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS") LocalDateTime dateTime, Long numberOfItems, Long numberOfPage) {
+        Map<String, Object> res = new LinkedHashMap<>();
         res.put("status", status);
+        res.put("body", responseObj);
         res.put("message", message);
-        res.put("data", responseObj);
+        res.put("date", dateTime);
         res.put("version", VERSION);
-        res.put("responseDate", dateTime);
+        res.put("currentPage", numberOfPage);
+        res.put("totalPages", numberOfItems);
 
         return new ResponseEntity<>(res, HttpStatus.valueOf(status));
     }
