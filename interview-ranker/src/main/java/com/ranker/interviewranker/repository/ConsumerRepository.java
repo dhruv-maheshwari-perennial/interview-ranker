96ec975fd3a9cdcbae0efd66b6a4f6019c495044
Dhruv Maheshwari <dhruv.maheshwari@perennialsys.com>
2022-10-21 14:00:59 +0530

    Added remove query

--- interview-ranker/src/main/java/com/ranker/interviewranker/repository/ConsumerRepository.java
+++ interview-ranker/src/main/java/com/ranker/interviewranker/repository/ConsumerRepository.java
@@ -3,11 +3,15 @@ package com.ranker.interviewranker.repository;
 import com.ranker.interviewranker.domain.model.InterviewDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ConsumerRepository extends MongoRepository<InterviewDetails, String> {
 
     @Query("{'candidateName': ?0, 'interviewTrack': ?1}")
     List<InterviewDetails> findInterviewDetailsByCandidateName(String candidateName, String interviewTrackEnum);
+
+    void removeInterviewDetailsByCandidateNameAndInterviewTrack(String candidateName, String interviewTrack);
 }
