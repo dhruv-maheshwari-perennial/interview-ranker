93255d9e243200ee7f9356b502b9e61baf52b150
Dhruv Maheshwari <dhruv.maheshwari@perennialsys.com>
2022-10-21 14:00:37 +0530

    Added setter annotation

--- interview-ranker/src/main/java/com/ranker/interviewranker/domain/model/InterviewDetails.java
+++ interview-ranker/src/main/java/com/ranker/interviewranker/domain/model/InterviewDetails.java
@@ -3,11 +3,13 @@ package com.ranker.interviewranker.domain.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @ToString
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -20,23 +22,19 @@ public class InterviewDetails {
    private String interviewTrack;
    private String panelName;
 
-   public void setDateAndTime(LocalDateTime dateAndTime) {
-      this.dateAndTime = dateAndTime;
-   }
-
-   public void setCandidateName(String candidateName) {
+   public void setCandidateName(@NotNull String candidateName) {
       this.candidateName = candidateName.toLowerCase();
    }
 
-   public void setRecruiterName(String recruiterName) {
+   public void setRecruiterName(@NotNull String recruiterName) {
       this.recruiterName = recruiterName.toLowerCase();
    }
 
-   public void setInterviewTrack(String interviewTrack) {
+   public void setInterviewTrack(@NotNull String interviewTrack) {
       this.interviewTrack = interviewTrack.toLowerCase();
    }
 
-   public void setPanelName(String panelName) {
+   public void setPanelName(@NotNull String panelName) {
       this.panelName = panelName.toLowerCase();
    }
 }
